141129a6dea7a844911297376f5cd19ca57be495
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2025-10-02 01:23:19 +0800

    Add models for Category, Item, Order, OrderItem, and Role with relationships; update User model to include SoftDeletes and new fields

---
+++ app/Models/Item.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Item extends Model
+{
+    use SoftDeletes;
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'img', 'is_active', 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+}
---
+++ app/Models/Order.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    protected $fillable = ['order_code','user_id', 'subtotal', 'tax', 'grandtotal', 'status', 'table_number', 'payment_method' , 
+    'note' , 'created_at', 'updated_at'];
+    protected $dates = ['deleted_at'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+    public function orderItems()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
