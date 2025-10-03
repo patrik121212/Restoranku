4081522cecba166cf7c9ad00ffa931d17cf75f96
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2025-10-04 04:58:02 +0800

    Update ItemFactory to use predefined image URLs instead of random image generation

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -13,7 +13,8 @@ class ItemFactory extends Factory
             'category_id' => $this->faker->numberBetween(1, 2),
             'price' => $this->faker->randomFloat(2, 1000, 10000),
             'description' => $this->faker->text(),
-            'img' => $this->faker->imageUrl(),
+            'img' => fake()->randomElement(['https://plus.unsplash.com/premium_photo-1668146927669-f2edf6e86f6f', 'https://images.unsplash.com/photo-1526318896980-cf78c088247c'
+            , 'https://images.unsplash.com/photo-1658043186384-7add63d278fd','https://images.unsplash.com/photo-1641790048634-3133a49929e1']),
             'is_active' => $this->faker->boolean(),
         ];
     }
