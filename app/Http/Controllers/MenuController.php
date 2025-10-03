1f53010a600d7723211c903e0bf63fc3e346c47a
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2025-10-04 04:57:29 +0800

    Add cart functionality to MenuController with addToCart method

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -17,4 +17,36 @@ class MenuController extends Controller
        $items = Item::where('is_active', 1)->orderBy('name', 'asc')->get();
         return view('customer.menu', compact('items', 'tableNumber'));
     }
+
+    public function cart(){
+        $cart = Session::get('cart');
+        return view('customer.cart', compact('cart'));
+    }
+
+    public function addToCart(Request $request)
+    {
+        $menuId = $request->input('id');
+        $menu = Item::find($menuId);
+        if (!$menu) {
+            return response()->json([ 'status'=> 'error',
+            'message' => 'Menu tidak ditemukan']);
+        }
+        $cart = Session::get('cart');
+        if (isset($cart[$menuId])) {
+            $cart[$menuId]['qty'] += 1;
+        } else {
+            $cart[$menuId] = [
+                'id' => $menu->id,
+                'name' => $menu->name,
+                'price' => $menu->price,
+                'image' => $menu->img,
+                'qty' => 1,
+            ];  }
+        Session::put('cart', $cart);
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Berhasil ditambahkan ke keranjang',
+            'cart' => $cart
+        ]);
+    }
 }
