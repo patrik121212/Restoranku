32d394025e2146bf7531e8b20b883287d9f4fdcc
patrik121212 <119336295+patrik121212@users.noreply.github.com>
2025-11-29 01:04:17 +0800

    Enhance user creation: add validation for input fields and improve form layout

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,9 +12,9 @@ class UserController extends Controller
      * Display a listing of the resource.
      */
     public function index()
-   {
-        // Fetch all users from the database
-        $users = User::whereHas('role', function($query) {
+    {
+        // mengambil data user dengan role selain customer
+        $users = User::whereHas('role', function ($query) {
             $query->where('role_name', '!=', 'customer');
         })->orderBy('fullname')->get();
 
@@ -37,12 +37,34 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // Validate the request data
+        $validatedData = $request->validate([
+            'fullname' => 'required|string|max:255',
+            'username' => 'required|string|max:255|unique:users,username',
+            'phone' => 'required|string|max:20|unique:users,phone',
+            'email' => 'required|string|email|max:255|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',
+            'role_id' => 'required|exists:roles,id',
+        ], [
+            'fullname.required' => 'Full name is required.',
+            'username.required' => 'Username is required.',
+            'phone.required' => 'Phone number is required.',
+            'email.required' => 'Email is required.',
+            'password.required' => 'Password is required.',
+            'role_id.required' => 'Role is required.',
+            'password.confirmed' => 'Password confirmation does not match.',
+            'username.unique' => 'Username has already been taken.',
+            'phone.unique' => 'Phone number has already been taken.',
+            'email.unique' => 'Email has already been taken.'
+        ]);
+        // validate data
+        $validatedData['password'] = bcrypt($validatedData['password']);
+
+        User::create($validatedData);
+        return redirect()->route('users.index')->with('success', 'User created successfully.');
     }
 
-    /**
-     * Display the specified resource.
-     */
+
     public function show(string $id)
     {
         //
